556ca60ca617c118cc038b27309473d538e186d8
anubhabx <anubhabdebnath98@gmail.com>
2026-01-21 22:50:56 +0530

    fix (web): fixed the dither background being rendered as a block.

--- apps/web/app/(auth)/layout.tsx
+++ apps/web/app/(auth)/layout.tsx
@@ -9,16 +9,17 @@ const AuthLayout = ({ children }: Props) => {
   return (
     <div className="h-screen w-full flex items-center justify-center relative">
       {children}
-      <Dither
-        waveColor={[0.5, 0.6, 0.5]}
-        disableAnimation={false}
-        enableMouseInteraction
-        mouseRadius={0.3}
-        colorNum={16}
-        waveAmplitude={0}
-        waveFrequency={0}
-        waveSpeed={0.01}
-      />
+      <div className="absolute inset-0 -z-10">
+        <Dither
+          disableAnimation={false}
+          enableMouseInteraction
+          mouseRadius={0.3}
+          colorNum={16}
+          waveAmplitude={0}
+          waveFrequency={0}
+          waveSpeed={0.01}
+        />
+      </div>
     </div>
   );
 };
